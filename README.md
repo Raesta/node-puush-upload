73b1d24a154c09003e55612264a88cd0ee1cce42
Raesta <dvs75019@gmail.com>
2016-04-07 00:10:46 +0200

    Initial commit of code

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 node-puush-upload
+======
+
+Node package for uploading screenshots to [puush](http://puush.me/).
+
+---
+* [Puush](#puushapikey) ([apiKey]) - Constructor, apiKey is required.
+* [simple](#simplefilename--callback) (filename [, callback]) - Upload file. Maximum filesize for free account: 272629051 bytes
+
+### Example
+```javascript
+var Puush = require('puush');
+
+var puush = new Puush('YOURAPIKEY');
+```
+
+---
+### `Puush(apiKey)`
+Constructor, apiKey is required.
+
+---
+#### `simple(filename, callback)`
+Upload a file.  
+Maximum file size for free account: 260MB-708B-filename.length,  
+if your filename is only 1 character, then it is: 272629051 bytes
+
+##### Result from callback:
+```javascript
+{
+  id: 123478,  // uploaded file id
+  url: 'http://puu.sh/x/y' // download url
+}
+```
+
+### Todo
+- Multi (dir [, callback]) // Multiple upload with folder
---
+++ index.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var request = require('request');
+
+module.exports = Puush;
+
+function Puush(apiKey) {
+  if (typeof apiKey === 'undefined')
+    throw new Error('the api key is not defined.');
+  else
+    this.API_KEY = apiKey;
+  this.API_URL = "https://puush.me/api/up";
+};
+
+Puush.prototype.single = function(path, callback) {
+  var self = this;
+  if (typeof path === 'undefined')
+    throw new Error('path file is not defined.');
+  else {
+    upload(path, self.API_URL, self.API_KEY, function(data) {
+      if (typeof callback === 'undefined') return data;
+      else callback(data);
+    });
+  }
+};
+
+function upload(file, apiUrl, apiKey, callback) {
+  var path = require('path').dirname(require.main.filename) + file;
+  var headers = { 'User-Agent': 'node-puush-upload/0.1 (Npm module for uploading screenshots to puush; https://github.com/Raesta/node-puush-upload)'};
+  var formData = { 'k': apiKey, 'z': 'poop', 'f': fs.createReadStream(path) };
+  request.post({url: apiUrl, formData: formData}, function optionalCallback(err, httpResponse, body) {
+    if (err) {
+      throw new Error('upload failed: ' + err);
+    } else {
+      var data = body.split(',');
+      callback({ id: data[2], url: data[1] });
+    }
+  });
+}
