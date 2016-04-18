2735dd4d7d35734b1946799cf3e9555d4a75ef98
Raesta <dvs75019@gmail.com>
2016-04-18 21:12:51 +0200

    Added some comments for helping developers

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var request = require('request');
 
 module.exports = Puush;
 
+/**
+ * Puush API handler
+ * @constructor
+ * @param {String} apiKey API key
+ */
 function Puush(apiKey) {
   if (typeof apiKey === 'undefined')
     throw new Error('the api key is not defined.');
@@ -12,9 +17,27 @@ function Puush(apiKey) {
   this.IGNORED_FILES = ['.DS_Store', '.AppleDouble', '.LSOverride', 'Thumbs.db', 'ehthumbs.db'];
 }
 
+/**
+ * Version of this application
+ * @constant
+ * @type {String}
+ */
 Puush.prototype.version = '1.0.0';
+
+/**
+ * Base URL of puush API
+ * @private
+ * @constant
+ * @type {String}
+ */
 Puush.prototype.API_URL = 'https://puush.me/api/up';
 
+/**
+ * Upload simple file
+ * @param  {String}   path     path of the file
+ * @param  {Function} [callback] response handler callback
+ * @return {Object} id and url of the uploded file
+ */
 Puush.prototype.single = function(path, callback) {
   var self = this;
   if (typeof path === 'undefined')
@@ -29,6 +52,12 @@ Puush.prototype.single = function(path, callback) {
   }
 };
 
+/**
+ * Upload multiple files from a folder
+ * @param  {String}   dirName  name of the folder
+ * @param  {Function} [callback] response handler callback
+ * @return {Array}      list of all uploaded files
+ */
 Puush.prototype.multi = function(dirName, callback) {
   var self = this;
   if (typeof dirName === 'undefined')
@@ -47,6 +76,12 @@ Puush.prototype.multi = function(dirName, callback) {
   }
 };
 
+/**
+ * Get all files in a folder
+ * @param  {String}   dirName     name of folder
+ * @param  {Array}   ignoredFiles ignored files in list
+ * @param  {Function} [callback]  response handler callback
+ */
 function getFiles(dirName, ignoredFiles, callback) {
   fs.readdir(dirName, function(err, files) {
     if (err) throw new Error(err);
@@ -61,6 +96,13 @@ function getFiles(dirName, ignoredFiles, callback) {
   });
 }
 
+/**
+ * Upload the file with api in Puush.me with your API Key
+ * @param  {String}   file     name of file
+ * @param  {String}   apiUrl   API Url
+ * @param  {String}   apiKey   API Key
+ * @param  {Function} [callback] response handler callback
+ */
 function upload(file, apiUrl, apiKey, callback) {
   var path = require('path').dirname(require.main.filename) + file;
   var headers = { 'User-Agent': 'node-puush-upload/0.1 (Npm module for uploading screenshots to puush; https://github.com/Raesta/node-puush-upload)'};
